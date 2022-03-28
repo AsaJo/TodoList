683fb13412335fd009a5c916e25d35ecee7a078b
AsaJo <101865458+AsaJo@users.noreply.github.com>
2022-03-28 07:56:07 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+//Select DOM 
+
+// "Step 1"
+// "Grabb the todo-input"
+// "Grabb the todo-button"
+// "Grabb the todo-list"
+// "Grabb the filter-todo"
+
+//Event Listeners
+
+//Step 2 Add Eventlistners here like add todo
+
+//Functions // Step 3 create your li
+function addTodo(event) {
+// Prevent form from submitting or reloading Step 4
+// Create a todo div-> and add todo          Step 4
+// Create li                                 Step 4
+// Create Completed Button
+// Create trash button
+// attach final Todo
+} 
+
+
+function deleteTodo(event) {}
+// e.target.parentElement.remove();
+
+function filterTodo(event) {}
+// use switch here as we have all, completed and uncompleted
